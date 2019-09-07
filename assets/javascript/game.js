96e2e6eb7b48ef5d7044206835b6cb802e3e7b22
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-09-07 17:44:13 -0400

    Started to lay out the actual html and did my pseudo coding for what needs to be done

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+// Need to make a header containing a jumbotron with the title of my game, to be done on my html
+// Will have an explanation of the rules below the jumbotron via the html
+// Need to place the images for the jewels so they display on the page
+// At the start of each game I need to have a random number that changes.
+// Each of the jewels will be given a different value at the start of each new game, you will not know the value until you click the jewel
+// In order to win this game you need to match your score to the random number so ill need to build a function
+// Have a display for wins and losses that will update when you win or lose
