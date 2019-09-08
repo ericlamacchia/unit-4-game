c5260cff978681f22c92c01caa295b1e9570cb85
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-09-07 20:11:24 -0400

    Started from scratch was making something overly complicated, I have the actual blocks for the jewel set up and have the process for genereating a random number as well as assigning a number to each space for the jewel.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,3 +5,37 @@
 // Each of the jewels will be given a different value at the start of each new game, you will not know the value until you click the jewel
 // In order to win this game you need to match your score to the random number so ill need to build a function
 // Have a display for wins and losses that will update when you win or lose
+// need to have my target value as 102 + 19
+
+
+var randomResult;
+var losses;
+var wins;
+
+randomResult = Math.floor(Math.random() * 102 ) + 19;
+
+
+
+$("#result").html('Random Number: ' + randomResult);
+
+for(var i = 0; i < 4; i++){
+    var random =Math.floor(Math.random() * 11) + 1;
+    console.log(random);
+    var jewel = $("<div>");
+        jewel.attr({
+            "class": 'jewel',
+            "data-random": random
+        });
+
+
+    $(".jewels").append(jewel);
+
+    }
+    
+    $(".jewel").on('click', function () {
+        console.log($(this));
+        
+    });
+
+    
+
