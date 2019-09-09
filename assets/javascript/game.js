cff156405d986ac02c5b11dfd0c2359fa9301be9
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-09-09 13:01:07 -0400

    Added my directions to the page and changed my text color as well, will try and make it look even fancier soon

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,20 +7,24 @@
 // Have a display for wins and losses that will update when you win or lose
 // need to have my target value as 102 + 19
 
-
+// Named my variables for the rest of the assignment
+// Made an array for my different jewel images
 var randomResult;
 var losses = 0;
 var wins = 0;
 var previous = 0;
 var images = ["assets/images/green gem.png", "assets/images/orange gem.png", "assets/images/redgem.png", "assets/images/yellow.png"]
 
+//This resets the value each time the game restarts
+
 var resetGame = function () {
 
     $(".jewels").empty();
+// This is the math for my highest possible number as well as my lowest possible number
 
     randomResult = Math.floor(Math.random() * 102) + 19;
 
-
+// Assigning that random number and adding it to my display
 
     $("#result").html('Random Number: ' + randomResult);
 
